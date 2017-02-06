fa11dd8b9ba75144528bfac885cffdabfc834fdc
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-06 15:55:17 +0000

    seperate book setup into test setup method

--- extension_spec.rb
+++ extension_spec.rb
@@ -4,21 +4,32 @@ require_relative('./extension')
 
 class TestExtension <  Minitest::Test
 
-  def test_new_library
-    book1 = { 
+  def setup
+    @book1 = { 
       title: "lord_of_the_rings",
       rental_details: { 
        student_name: "Jeff", 
        date: "01/12/16"
       }
     }
-    book2 = {
+    @book2 = {
       title: "the_hobbit",
       rental_details: {
         student_name: "Jeff",
         date: "01/12/16"
       }
     }
+    @book3 = {
+      title: "the_silmarillion",
+      rental_details: {
+        student_name: "",
+        date: ""
+      }
+    }
+  end
+
+
+  def test_new_library
     a_library = Library.new
     assert_equal( [], a_library.books)
 
