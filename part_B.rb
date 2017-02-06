eea44b6206ff0968fdeef74d8ceef957a93bce6e
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-06 14:47:28 +0000

    refactor with accessor methods

--- part_B.rb
+++ part_B.rb
@@ -1,22 +1,13 @@
 class Team
-  #attr_accessor :name, :players, :coach
+  attr_accessor :name, :players, :coach
 
-  def initialize(name, players, coach)
-    @name = name
+  def initialize(team_name, players, coach)
+    @name = team_name
     @players = players
     @coach = coach
   end
 
-  def get_name
-    return @name  
-  end
-  def get_players 
-    return @players
-  end
-  def get_coach
-    return @coach
-  end
-
+  
   def set_coach(new_coach)
     @coach = new_coach
   end
