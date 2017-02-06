32c07e7732161dbc2874584b87e68e7e3f284707
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-06 14:34:16 +0000

    start part B, test for Team.new

--- part_B_spec.rb
+++ part_B_spec.rb
@@ -8,8 +8,9 @@ class TestPartB <  Minitest::Test
     team_name = "Crystal Palace"
     team_players = [ 'alpha', 'bravo', 'charlie']
     team_coach = "Delta"
-    # a_team = Team.new(team_name, team_players, team_coach)
-    # assert_equal("Douglas", student.name)
-    # assert_equal(10, student.cohort)
+    a_team = Team.new(team_name, team_players, team_coach)
+    assert_equal("Crystal Palace", a_team.name)
+    assert_equal( team_players, a_team.players)
+    assert_equal(team_coach, a_team.coach)
   end
 end
