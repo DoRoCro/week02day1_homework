83022d35bfa5ab499cd1cf34a0ac58e4ee7e5f5f
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-06 14:14:59 +0000

    checking file saved...

--- part_A_spec.rb
+++ part_A_spec.rb
@@ -6,7 +6,7 @@ class TestPartA <  Minitest::Test
 
   def test_student
     student = Student.new("Douglas", 10)
-    assert_equal("Douglas",student.name)
+    assert_equal("Douglas", student.name)
     assert_equal(10, student.cohort)
   end
 
@@ -18,8 +18,9 @@ class TestPartA <  Minitest::Test
     assert_equal("Rob", student.name)    
   end
 
-  def test_student_talks
-    
+  def test_student_say_my_name
+    student = Student.new("Carol", 10)
+    assert_equal("My name is Carol",student.say_my_name)
   end
 
 end
